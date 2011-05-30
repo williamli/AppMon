f1012db25b794a745c8c92e0092b3e4527c97bee
siuying <siu.ying@gmail.com>
2011-05-30 21:22:04 +0800

    add Store model

---
+++ AppMon/Models/Store.h
@@ -0,0 +1,20 @@
+//
+//  Store.h
+//  AppMon
+//
+//  Created by Chong Francis on 11年5月30日.
+//  Copyright 2011年 Ignition Soft Limited. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface Store : NSObject {
+@private
+    
+}
+
+@property (nonatomic, retain) NSString* name;
+@property (nonatomic, retain) NSString* storefront;
+
+@end
---
+++ AppMon/Models/Store.m
@@ -0,0 +1,41 @@
+//
+//  Store.m
+//  AppMon
+//
+//  Created by Chong Francis on 11年5月30日.
+//  Copyright 2011年 Ignition Soft Limited. All rights reserved.
+//
+
+#import "Store.h"
+
+@implementation Store
+
+@synthesize name=_name, storefront=_storefront;
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+    }
+    return self;
+}
+
+- (id)initWithName:(NSString*)name storefront:(NSString*)storefront
+{
+    self = [super init];
+    if (self) {
+        self.name = name;
+        self.storefront = storefront;
+    }
+    return self;
+}
+
+
+- (void)dealloc
+{
+    self.name = nil;
+    self.storefront = nil;
+    [super dealloc];
+}
+
+@end
